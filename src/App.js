0355a46b229736beafcbcf62968ed6831454df52
Jessica Cohen <jessica.cohen@edu.escpeurope.eu>
2017-10-25 16:48:26 +0200

    Modif après Pierre

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import MyComponent from './Component';
+import Title from './Title';
 
 class App extends Component {
   render() {
     const list = ['a', 'b', 'c']
     return (
-      <div className="App">
-        {list.map(function(l){ return (<MyComponent title={l} />)})}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="App">
+          {list.map(function(l){ return (<Title title={l} />)})}
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        <div>Salut les gars</div>
       </div>
     );
   }
 }
 
-export default App;
 
+
+export default App;
