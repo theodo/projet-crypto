29ceac154d8db0504f68f2d468eb93646148eb29
ThierryCols <thierrycolsenet@gmail.com>
2017-10-26 11:11:31 +0200

    :art: first stylesheet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import Title from './Title';
+import './app.css'
+import Title from './components/Title';
+import ethImg from './assets/eth.png'
 
 class App extends Component {
   render() {
-    const list = ['JE SUIS PIERRE', 'b', 'c']
+    const list = ['J\'aime', 'les', 'Cryptos']
     return (
       <div>
         <div className="App">
-          {list.map(function(l){ return (<Title title={l} />)})}
+          <img className="ethlogo" src={ethImg} alt="ETH Logo" />
+          {
+            list.map((string) => (<Title title={string}/>))
+          }
           <p className="App-intro">
             To get started, edit <code>src/App.js</code> and save to reload.
           </p>
@@ -17,7 +22,5 @@ class App extends Component {
     );
   }
 }
-
-
 
 export default App;
