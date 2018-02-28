8fd7003993265172031036f0190ec5df62b67d9b
PtotheM <36155026+PtotheM@users.noreply.github.com>
2018-02-28 18:17:24 +0100

    probleme de translation résolu

--- src/components/joinedChart.js
+++ src/components/joinedChart.js
@@ -1,102 +1,103 @@
-
 import React, { Component } from 'react';
-import axios from 'axios'
+
 import { Line } from 'react-chartjs-2';
 
 class Joinedchart extends Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      chartData1:{
-            labels: [],
-            datasets: [],
-        },
-    };
-  }
-  componentDidMount() {
-      let chartData1=  {
-                labels: [],
-                datasets: [],
-            };
-        axios('https://poloniex.com/public?command=returnchartData&currencyPair=USDT_BTC&start=1486512000&end=1516752000&period=86400')
-      .then((response) => {
+constructor(props){
+   super(props);
+   console.log(props.chartData1, 'chartData')
+   this.state = {
+     chartData1:{},
+   }
+ }
+/*
+ componentDidMount() {
 
-        const Datapoloniex = response.data;
-        chartData1 = {
-            labels: Datapoloniex.map(k => k.date),
-            datasets: [
-             {
-                  label: 'My First dataset',
-                  fill: false,
-                  lineTension: 0.1,
-                  backgroundColor: 'rgba(75,192,192,0.4)',
-                  borderColor: 'rgba(75,192,192,1)',
-                  borderCapStyle: 'butt',
-                  borderDash: [],
-                  borderDashOffset: 0.0,
-                  borderJoinStyle: 'miter',
-                  pointBorderColor: 'rgba(75,192,192,1)',
-                  pointBackgroundColor: '#fff',
-                  pointBorderWidth: 1,
-                  pointHoverRadius: 5,
-                  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                  pointHoverBorderColor: 'rgba(220,220,220,1)',
-                  pointHoverBorderWidth: 2,
-                  pointRadius: 1,
-                  pointHitRadius: 10,
-                  data: Datapoloniex.map(d => d.open),
+    axios('https://poloniex.com/public?command=returnChartData&currencyPair=USDT_BTC&start=1486512000&end=1516752000&period=86400')
+     .then((response) => {
 
-        }]
-      }
-      })
-     axios('https://api.gdax.com/products/BTC-USD/candles?granularity=86400')
-      .then((response) => {
+       const Data = response.data;
+       const chartData = {
+           labels: Data.map(k => k.date),
+           datasets: [
+            {
+                 label: 'My First dataset',
+                 fill: false,
+                 lineTension: 0.1,
+                 backgroundColor: 'rgba(75,192,192,0.4)',
+                 borderColor: 'rgba(75,192,192,1)',
+                 borderCapStyle: 'butt',
+                 borderDash: [],
+                 borderDashOffset: 0.0,
+                 borderJoinStyle: 'miter',
+                 pointBorderColor: 'rgba(75,192,192,1)',
+                 pointBackgroundColor: '#fff',
+                 pointBorderWidth: 1,
+                 pointHoverRadius: 5,
+                 pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                 pointHoverBorderColor: 'rgba(220,220,220,1)',
+                 pointHoverBorderWidth: 2,
+                 pointRadius: 1,
+                 pointHitRadius: 10,
+                 data: Data.map(d => d.open)},
+     {   label: "second set",
+         data: [11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300,11300]
+     }
+ ]
 
-          const Datacoinbase = response.data.reverse();
-          chartData1.datasets.push(
-              {
-                  label: 'Price BTC/USD',
-                  fill: false,
-                  lineTension: 0.1,
-                  backgroundColor: 'blue',
-                  borderColor: 'blue',
-                  borderCapStyle: 'butt',
-                  borderDash: [],
-                  borderDashOffset: 0.0,
-                  borderJoinStyle: 'miter',
-                  pointBorderColor: 'blue',
-                  pointBackgroundColor: '#fff',
-                  pointBorderWidth: 1,
-                  pointHoverRadius: 5,
-                  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                  pointHoverBorderColor: 'rgba(220,220,220,1)',
-                  pointHoverBorderWidth: 2,
-                  pointRadius: 1,
-                  pointHitRadius: 10,
-                  data: Datacoinbase.map(transac => transac[3]),
-              }
-          )
-      }).then( () =>
-        this.setState({ chartData1 })
-     )
-      }
+           /*labels: ['Jan', 'Feb', 'Mar', 'Ap','Mai']
+           datasets:[
+                   {
+                       label: 'Price',
+                       fill: false,
+                       lineTension: 0.1,
+                       backgroundColor: 'rgba(75,192,192,0.4)',
+                       borderColor: 'rgba(75,192,192,1)',
+                       borderCapStyle: 'butt',
+                       borderDash: [],
+                       borderDashOffset: 0.0,
+                       borderJoinStyle: 'miter',
+                       pointBorderColor: 'rgba(75,192,192,1)',
+                       pointBackgroundColor: '#fff',
+                       pointBorderWidth: 1,
+                       pointHoverRadius: 5,
+                       pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                       pointHoverBorderColor: 'rgba(220,220,220,1)',
+                       pointHoverBorderWidth: 2,
+                       pointRadius: 1,
+                       pointHitRadius: 10,
 
-  render() {
-    return (
-      <div className="chart">
-        <Line
-          data={this.props.chartData1}
-          width={10}
-          height={5}
-          redraw={true}
-          options={{
-            maintainAspectratio: false,
-          }}
-        />
-      </div>
-    );
-  }
+                       //data: Data.map(d => d.rate),
+                       data:[1,1,1,1,1],
+                       backgroundColor: 'lightblue',
+
+                   },
+                                           label: "second set",
+
+                       data: [2, 2, 2, 2],
+                       backgroundColor: 'lightblue',
+
+                   }
+               //]
+           }
+
+       //this.setState({ chartData });
+     });
+ }
+*/
+render() {
+   return (
+     <div className="chart">
+       <Line
+         data={this.props.chartData1}
+         // width={10}
+          //height={20}
+         options={{}}
+       />
+     </div>
+   );
+}
 }
 
 export default Joinedchart;
